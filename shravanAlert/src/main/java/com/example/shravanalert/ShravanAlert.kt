70967d2d08712c075955fd36f98e5cf7bf9cf4d0
Shravan Kushwaha <kushwahashravan75@gmail.com>
2023-04-19 17:53:40 +0530

    Update ShravanAlert.kt

--- shravanAlert/src/main/java/com/example/shravanalert/ShravanAlert.kt
+++ shravanAlert/src/main/java/com/example/shravanalert/ShravanAlert.kt
@@ -3,11 +3,11 @@ package com.example.shravanalert
 import android.content.Context
 import android.widget.Toast
 
-class ShravanAlert {
-    companion object {
-        fun alert(context: Context, msg: String) {
-            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
-        }
+object ShravanAlert {
+
+    fun alert(context: Context, msg: String) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+
     }
 
 }
